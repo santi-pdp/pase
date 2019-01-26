373200df53bf695528d2a63672a59081e00bdc14
spascual <santi.pdp@gmail.com>
2019-01-26 12:20:13 +0100

    feat: new SincNetConv_fast adapted and inserted in the FE

--- waveminionet/models/modules.py
+++ waveminionet/models/modules.py
@@ -466,6 +466,154 @@ class SincConv(nn.Module):
                        stride=self.stride)
         return out
 
+class SincConv_fast(nn.Module):
+    """Sinc-based convolution
+    Parameters
+    ----------
+    in_channels : `int`
+        Number of input channels. Must be 1.
+    out_channels : `int`
+        Number of filters.
+    kernel_size : `int`
+        Filter length.
+    sample_rate : `int`, optional
+        Sample rate. Defaults to 16000.
+    Usage
+    -----
+    See `torch.nn.Conv1d`
+    Reference
+    ---------
+    Mirco Ravanelli, Yoshua Bengio,
+    "Speaker Recognition from raw waveform with SincNet".
+    https://arxiv.org/abs/1808.00158
+    """
+
+    @staticmethod
+    def to_mel(hz):
+        return 2595 * np.log10(1 + hz / 700)
+
+    @staticmethod
+    def to_hz(mel):
+        return 700 * (10 ** (mel / 2595) - 1)
+
+    def __init__(self, in_channels, out_channels, kernel_size,
+                 stride=1, padding='VALID', pad_mode='reflect',
+                 dilation=1, bias=False, groups=1,
+                 sample_rate=16000, min_low_hz=50, min_band_hz=50):
+
+        super(SincConv_fast,self).__init__()
+
+        if in_channels != 1:
+            #msg = (f'SincConv only support one input channel '
+            #       f'(here, in_channels = {in_channels:d}).')
+            msg = "SincConv only support one input channel (here, in_channels = {%i})" % (in_channels)
+            raise ValueError(msg)
+
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        
+        # Forcing the filters to be odd (i.e, perfectly symmetrics)
+        if kernel_size%2==0:
+            self.kernel_size=self.kernel_size+1
+            
+        self.stride = stride
+        self.padding = padding
+        self.pad_mode = pad_mode
+        self.dilation = dilation
+
+        if bias:
+            raise ValueError('SincConv does not support bias.')
+        if groups > 1:
+            raise ValueError('SincConv does not support groups.')
+
+        self.sample_rate = sample_rate
+        self.min_low_hz = min_low_hz
+        self.min_band_hz = min_band_hz
+
+        # initialize filterbanks such that they are equally spaced in Mel scale
+        low_hz = 30
+        high_hz = self.sample_rate / 2 - (self.min_low_hz + self.min_band_hz)
+
+        mel = np.linspace(self.to_mel(low_hz),
+                          self.to_mel(high_hz),
+                          self.out_channels + 1)
+        hz = self.to_hz(mel)
+        
+
+        # filter lower frequency (out_channels, 1)
+        self.low_hz_ = nn.Parameter(torch.Tensor(hz[:-1]).view(-1, 1))
+
+        # filter frequency band (out_channels, 1)
+        self.band_hz_ = nn.Parameter(torch.Tensor(np.diff(hz)).view(-1, 1))
+
+        # Hamming window
+        #self.window_ = torch.hamming_window(self.kernel_size)
+        n_lin=torch.linspace(0, (self.kernel_size/2)-1, steps=int((self.kernel_size/2))) # computing only half of the window
+        self.window_=0.54-0.46*torch.cos(2*math.pi*n_lin/self.kernel_size);
+
+
+        # (kernel_size, 1)
+        n = (self.kernel_size - 1) / 2.0
+        self.n_ = 2*math.pi*torch.arange(-n, 0).view(1, -1) / self.sample_rate # Due to symmetry, I only need half of the time axes
+
+ 
+
+
+    def forward(self, waveforms):
+        """
+        Parameters
+        ----------
+        waveforms : `torch.Tensor` (batch_size, 1, n_samples)
+            Batch of waveforms.
+        Returns
+        -------
+        features : `torch.Tensor` (batch_size, out_channels, n_samples_out)
+            Batch of sinc filters activations.
+        """
+
+        self.n_ = self.n_.to(waveforms.device)
+
+        self.window_ = self.window_.to(waveforms.device)
+
+        low = self.min_low_hz  + torch.abs(self.low_hz_)
+        
+        high = torch.clamp(low + self.min_band_hz + torch.abs(self.band_hz_),self.min_low_hz,self.sample_rate/2)
+        band=(high-low)[:,0]
+        
+        f_times_t_low = torch.matmul(low, self.n_)
+        f_times_t_high = torch.matmul(high, self.n_)
+		# Equivalent of Eq.4 of the reference paper (SPEAKER RECOGNITION FROM RAW WAVEFORM WITH SINCNET). 
+        # I just have expanded the sinc and simplified the terms. This way I avoid several useless computations.
+        band_pass_left=((torch.sin(f_times_t_high)-torch.sin(f_times_t_low))/(self.n_/2))*self.window_  
+        band_pass_center = 2*band.view(-1,1)
+        band_pass_right= torch.flip(band_pass_left,dims=[1])
+        
+        
+        band_pass=torch.cat([band_pass_left,band_pass_center,band_pass_right],dim=1)
+
+        
+        band_pass = band_pass / (2*band[:,None])
+        
+
+        self.filters = (band_pass).view(
+            self.out_channels, 1, self.kernel_size)
+
+        x = waveforms 
+
+        if self.padding == 'SAME':
+            if self.stride > 1:
+                x_p = F.pad(x, (self.kernel_size // 2 - 1,
+                                self.kernel_size // 2), mode=self.pad_mode)
+            else:
+                x_p = F.pad(x, (self.kernel_size // 2,
+                                self.kernel_size // 2), mode=self.pad_mode)
+        else:
+            x_p = x
+
+        return F.conv1d(x_p, self.filters, stride=self.stride,
+                        padding=0, dilation=self.dilation,
+                        bias=None, groups=1) 
+
 
 class FeBlock(NeuralBlock):
 
@@ -486,11 +634,12 @@ class FeBlock(NeuralBlock):
         if sincnet:
             # only one-channel signal can be analyzed
             assert num_inputs == 1, num_inputs
-            self.conv = SincConv(fmaps,
-                                 kwidth, sr,
-                                 padding='SAME',
-                                 stride=stride,
-                                 pad_mode=pad_mode)
+            self.conv = SincConv_fast(1, fmaps,
+                                      kwidth, 
+                                      sample_rate=sr,
+                                      padding='SAME',
+                                      stride=stride,
+                                      pad_mode=pad_mode)
         else:
             self.conv = nn.Conv1d(num_inputs,
                                   fmaps,
