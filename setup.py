ff602c7ac9d2305b704e09c48102e6bd1a5288ab
spascual <santi.pdp@gmail.com>
2019-06-07 12:02:21 +0200

    fix: setup installing models now

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='PASE',
-    version='0.1dev',
-    packages=['pase',],
+    version='0.1-dev',
+    packages=['pase', 'pase.models'],
+    licence='MIT'
 )
