ea86cd46138f361904637cf8de691800c3418665
spascual <santi.pdp@gmail.com>
2019-06-28 16:34:59 +0200

    feat: runnable train resnet cfg: workers for resnet

---
+++ run_pase_resblocks_len20000_train.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python -u train.py --batch_size 32 --epoch 50 --save_path ckpt_PASE_resnet_newMI \
+       	--num_workers 4 --warmup 10000000 --net_cfg cfg/workers_resnet.cfg \
+	--fe_cfg cfg/PASE_resnet.cfg --do_eval --data_cfg data/librispeech_data_50h.cfg \
+	--min_lr 0.0005 --fe_lr 0.0005 --data_root data/LibriSpeech_50h/all/ \
+	--stats data/librispeech_50h_stats.pkl --lrdec_step 30 --lrdecay 0.5 \
+	--dataset LibriSpeechSegTupleWavDataset --chunk_size 20000 --cache_on_load
