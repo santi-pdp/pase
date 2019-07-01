a63776419d2beca490301eec5ca4ea321a04b335
joaomonteirof <joaomonteirof@gmail.com>
2019-07-01 14:53:07 -0400

    included data prep scripts for spk and lang id

---
+++ downstream_prep/get_voxforge_lid_data.py
@@ -0,0 +1,105 @@
+### from: https://github.com/PiSchool/spoken-language-id/blob/master/voxforge.py
+
+
+import os
+import re
+import csv
+import tarfile
+import argparse
+from collections import Counter
+from collections import OrderedDict
+
+import wget
+import requests
+
+
+def make_args():
+	parser = argparse.ArgumentParser(description="VoxForge dataset downloader.")
+	parser.add_argument('--per-user', default=3, type=int, help="Limit the number of recordings per user")
+	parser.add_argument('--per-user-archives', default=1, type=int, help="Limit the number of archives per user")
+	parser.add_argument('-d', '--output-dir', default='voxforge_samples', help="Directory to output wave files to")
+	parser.add_argument('-l', '--output-log', default='voxforge_samples.csv', help="Metadata about downloaded files")
+	return parser.parse_args()
+
+
+if __name__ == '__main__':
+	base_url = 'http://www.repository.voxforge1.org/downloads/{lang}/Trunk/Audio/Original/48kHz_16bit/{archive}'
+	languages = {
+		'Italian': 'it',
+		'French': 'fr',
+		'Portuguese': 'pt',
+		'German': 'de',
+		'English': 'SpeechCorpus',
+		'Spanish': 'es',
+	}
+
+	args = make_args()
+	os.makedirs(args.output_dir, exist_ok=True)
+
+	log_file = open(args.output_log, 'w')
+	log_csv = csv.writer(log_file, lineterminator='\n')
+	for lang_name, lang_code in languages.items():
+		download_url = base_url.format(lang=lang_code, archive='')
+
+		# Download an HTML page with archive names
+		print("Downloading archives for {}.".format(lang_name))
+		resp = requests.get(download_url)
+
+		# Generate a list of (archive_name, user_name) pairs
+		archives = re.findall(r'((\w+)-[\w-]+\.tgz)', resp.text)
+
+		# Remove duplicates, retain order
+		archives = OrderedDict.fromkeys(archives)
+
+		# Pick a single archive from each user
+		user_archives = Counter()
+		user_recordings = Counter()
+		for archive, user in archives:
+			if user_archives[user] >= args.per_user_archives:
+				# We have enough archives of this user
+				continue
+
+			user_archives[user] += 1
+
+			# Download an archive
+			download_url = base_url.format(lang=lang_code, archive=archive)
+
+			# Remove the downloaded archive
+			archive_file = '/tmp/foo.tgz'
+			try:
+				os.remove(archive_file)
+			except OSError:
+				pass
+
+			# Download the archive
+			wget.download(download_url, out=archive_file)
+			print()
+
+			per_archive_count = 0
+			with tarfile.open(archive_file, errorlevel=2) as tar:
+				for member in tar.getmembers():
+					# Find a member that is a wave file, e.g. archive-name/wav/it-0123.wav
+					wav_filename = re.match(r'([\w-]+)/.+/([\w-]+\.wav)', member.name)
+					if wav_filename is not None:
+						if user_recordings[user] >= args.per_user:
+							# We have enough files from this user
+							break
+
+						user_recordings[user] += 1
+						per_archive_count += 1
+						archive_name = wav_filename.group(1)
+
+						# Rename conveniently
+						member.name = '{archive}-{wav}'.format(archive=archive_name, wav=wav_filename.group(2))
+
+						# Skip if exists
+						if os.path.isfile(os.path.join(args.output_dir, member.name)):
+							continue
+
+						tar.extract(member, path=args.output_dir)
+						log_csv.writerow([member.name, lang_name, user, user_recordings[user]])
+						log_file.flush()
+			print("Extracted {} files from {}".format(per_archive_count, archive))
+
+		print("Recordings by {} users.".format(len(user_archives)))
+	log_file.close()
---
+++ downstream_prep/prep_voxceleb.py
@@ -0,0 +1,112 @@
+import argparse
+import os
+import glob
+import numpy as np
+import librosa
+
+def prep_rec(input_rec_path, out_rec_path, sr=16000, out_length_seconds=10):
+
+	try:
+
+		y, s = librosa.load(input_rec_path, sr=sr)
+
+		n_samples = sr*out_length_seconds
+
+		try:
+			ridx = np.random.randint(0, len(y)-n_samples)
+			librosa.output.write_wav(out_rec_path, y[ridx:(ridx+n_samples)], sr=sr)
+
+			y = y[ridx:(ridx+n_samples)]
+
+		except ValueError:
+
+			mul = int(np.ceil(n_samples/len(y)))
+			y = np.tile(y, (mul))[:n_samples]
+
+		librosa.output.write_wav(out_rec_path, y, sr=sr)
+
+		return True
+
+	except:
+
+		return False
+
+def clean_dir(dir_path):
+
+	file_list = glob.glob(dir_path+'/*.*')
+
+	if len(file_list)>0:
+		for file_ in file_list:
+			os.remove(file_)
+
+def dump_list(list_, path_):
+
+	with open(path_, 'w') as f:
+		for el in list_:
+			item = el + '\n'
+			f.write("%s" % item)
+
+if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser(description='Prep vox.')
+	parser.add_argument('--path-to-data', type=str, default='./data/')
+	parser.add_argument('--out-path', type=str, default='./')
+	parser.add_argument('--out-sr', type=int, default=16000)
+	parser.add_argument('--out-length', type=int, default=10)
+	parser.add_argument('--nspk', type=int, default=100)
+	args = parser.parse_args()
+
+	if not os.path.isdir(args.out_path+'train'):
+		os.mkdir(args.out_path+'train')
+
+	if not os.path.isdir(args.out_path+'test'):
+		os.mkdir(args.out_path+'test')
+
+	spk_list = np.random.choice(os.listdir(args.path_to_data), args.nspk, replace=False)
+
+	train_list, test_list = [], []
+	utt2spk = {}
+
+	for i, spk in enumerate(spk_list):
+
+		print('Speaker: {}'.format(spk))
+
+		folder_list = os.listdir(args.path_to_data + spk + '/')
+		rec_list = []
+
+		for folder in folder_list:
+			folder_recs = os.listdir(args.path_to_data + spk + '/' + folder + '/')
+
+			for rec in folder_recs:
+				rec_list.append(args.path_to_data + spk + '/' + folder + '/' + rec)
+
+		train_rec, test_rec = np.random.choice(rec_list, 2, replace=False)
+
+		trials=0
+		success=False
+		while not success and trials<args.ntrials:
+
+			clean_dir(args.path_to_data+'train')
+			clean_dir(args.path_to_data+'test')
+
+			train_rec, test_rec = np.random.choice(rec_list, 2, replace=False)
+
+			success = prep_rec(test_rec, args.out_path+'train/'+spk+'_-_'+train_rec.split('/')[-1], sr=args.out_sr, out_length_seconds=args.out_length) and prep_rec(test_rec, args.out_path+'test/'+spk+'_-_'+test_rec.split('/')[-1], sr=args.out_sr, out_length_seconds=args.out_length)
+
+			trials+=1
+
+		if trials>=args.ntrials:
+			print('Failed!!')
+			exit(1)
+
+		train_list.append(spk+'_-_'+train_rec.split('/')[-1])
+		test_list.append(spk+'_-_'+test_rec.split('/')[-1])
+		utt2spk[train_list[-1]] = i
+		utt2spk[test_list[-1]] = i
+
+	if not os.path.isdir(args.out_path+'lists'):
+		os.mkdir(args.out_path+'lists')
+
+	dump_list(train_list, args.out_path+'lists/train_list')
+	dump_list(test_list, args.out_path+'lists/test_list')
+	np.save(args.out_path+'lists/utt2spk', utt2spk)
