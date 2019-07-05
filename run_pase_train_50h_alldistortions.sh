6ff064f88c319a3f26dad0706d6d547bc867ee4f
spascual <santi.pdp@gmail.com>
2019-07-06 01:52:39 +0200

    feat: all dist sh runnable

---
+++ run_pase_train_50h_alldistortions.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#kernprof -v -l train.py --batch_size 32 --epoch 50 --save_path ckpt_PASE_50h_newMI_noW_denseskips_omologorevs_noise_tmp \
+python -u train.py --batch_size 32 --epoch 50 --save_path ckpt_PASE_50h_newMI_noW_denseskips_alldistortions \
+       	--num_workers 4 --warmup 10000000 --net_cfg cfg/workers.cfg \
+	--fe_cfg cfg/PASE_dense.cfg --do_eval --data_cfg data/librispeech_data_50h.cfg \
+	--min_lr 0.0005 --fe_lr 0.0005 --data_root data/LibriSpeech_50h/all/ \
+	--distortion_p 0.5 --dtrans_cfg cfg/distortions/all.cfg \
+	--stats data/librispeech_50h_stats.pkl --lrdec_step 30 --lrdecay 0.5 \
+	--chunk_size 32000 --random_scale
+
