386d0ced0ab49e42cb2fca129e207061273974cd
spascual <santi.pdp@gmail.com>
2019-07-06 01:41:52 +0200

    feat: random scale running

--- run_pase_train_50h_revs_noise.sh
+++ run_pase_train_50h_revs_noise.sh
@@ -8,5 +8,5 @@ python -u train.py --batch_size 32 --epoch 50 --save_path ckpt_PASE_50h_newMI_no
 	--min_lr 0.0005 --fe_lr 0.0005 --data_root data/LibriSpeech_50h/all/ \
 	--distortion_p 0.5 --dtrans_cfg cfg/distortions/omologo_revs_noise_overlap.cfg \
 	--stats data/librispeech_50h_stats.pkl --lrdec_step 30 --lrdecay 0.5 \
-	--chunk_size 32000
+	--chunk_size 32000 --random_scale
 
