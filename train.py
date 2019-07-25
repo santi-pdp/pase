a96f715cb12258dd821b995272e17b44977de1f8
Pawel Swietojanski <p.swietojanski@gmail.com>
2019-07-24 21:06:10 -0400

    handling none

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def str2bool(v):
   return v.lower() in ("yes", "true", "t", "1")
 
 def str2None(v):
-    if v.lower in ('none'):
+    if v.lower() in ('none'):
         return None
     return v
 
