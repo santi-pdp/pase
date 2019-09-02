746966b7a507452c2707b362a30384f7c099aa10
Pawel Swietojanski <p.swietojanski@gmail.com>
2019-09-02 15:38:31 +1000

    refactoring dropout

--- pase/models/modules.py
+++ pase/models/modules.py
@@ -119,70 +119,6 @@ def forward_activation(activation, tensor):
     else:
         return activation(tensor)
 
-class FixedPatternDropout(nn.Module):
-    def __init__(self, ninputs, p=0.5, 
-                  ratio_dropped=0.5, 
-                  drop_fixed_channels=True,
-                  drop_channels=False):
-        """Applies a fixed pattern of dropout for the whole training
-        session (i.e applies different only among pre-specified dimensions)
-        """
-        super(FixedPatternDropout, self).__init__()
-
-        if p < 0 or p > 1:
-            raise ValueError("dropout probability has to be between 0 and 1, "
-                             "but got {}".format(p))
-
-        self.ratio_dropped = ratio_dropped
-        self.drop_fixed_channels = drop_fixed_channels
-        self.drop_channels = drop_channels
-        self.fixed_dimsize = int(ninputs*ratio_dropped)
-
-        if self.drop_fixed_channels:
-            tot_idx = np.arange(ninputs)
-            sel_idx = np.sort(np.random.choice(tot_idx, 
-                              size=self.fixed_dimsize, replace=False))
-            self.dindexes = torch.LongTensor(sel_idx) 
-            print ("Selected dropout indices are: ", sel_idx)
-            self.p = p
-            self.p_scale = 1. / (1. - self.p)
-        else:
-            # when backing off to standard dropout, make sure the %
-            # of droppped units in a layer is comparable to the case
-            # where dropout is applied to a fixed subset only
-            self.p =  p*self.ratio_dropped
-            print ("Using std dropout, thus scaled p to {}".format(self.p))
-    
-    def forward(self, x):
-
-        if self.p == 0 or not self.training:
-            return x
-
-        if self.drop_fixed_channels and self.training:
-            self.dindexes = self.dindexes.to(x.device)
-            assert len(x.size()) == 3, (
-                "Expected to get 3 dimensional tensor, got {}"\
-                   .format(len(x.size()))
-            )
-            bsize, _, tsize = x.size()
-            #print (bsize, esize, tsize)
-            if self.drop_channels:
-                probs = torch.full(size=(bsize, self.fixed_dimsize),
-                                  fill_value=1.-self.p)
-                b = Binomial(total_count=1, probs=probs)
-                mask = b.sample()
-                x[:,self.dindexes,:] *= mask.view(bsize, 
-                                                   self.fixed_dimsize, -1)                            
-            else:
-                probs = torch.full(size=(bsize, self.fixed_dimsize, tsize), 
-                                 fill_value=1.-self.p)
-                b = Binomial(total_count=1, probs=probs)
-                mask = b.sample()
-                x[:,self.dindexes,:] *= mask 
-            return x * self.p_scale
-        else:
-            return F.dropout(x, p=self.p, training=self.training)
-
 class NeuralBlock(nn.Module):
 
     def __init__(self, name='NeuralBlock'):
@@ -460,24 +396,129 @@ class GConv1DBlock(NeuralBlock):
         #h = self.act(h)
         return h
 
+class PatternedDropout(nn.Module):
+    def __init__(self, emb_size, p=0.5, 
+                 dropout_mode=['fixed_rand'],
+                 ratio_fixed=None,
+                 range_fixed=None,
+                 drop_whole_channels=False):
+        """Applies a fixed pattern of dropout for the whole training
+        session (i.e applies different only among pre-specified dimensions)
+        """
+        super(PatternedDropout, self).__init__()
+
+        if p < 0 or p > 1:
+            raise ValueError("dropout probability has to be between 0 and 1, "
+                             "but got {}".format(p))
+
+        d_modes = ['std', 'fixed_rand', 'fixed_given']
+        assert dropout_mode in d_modes, (
+            "Expected dropout mode in {}, got {}".format(d_modes, dropout_mode)
+        )
+
+        self.drop_whole_channels = drop_whole_channels
+        self.dropout_fixed = False
+
+        if dropout_mode == 'fixed_rand':
+
+            self.dropout_fixed = True
+            assert ratio_fixed is not None, (
+                "{} needs 'ratio_fixed' arg set.".format(dropout_mode)
+            )
+            if ratio_fixed <= 0  or ratio_fixed > 1:
+                raise ValueError("{} mode needs 'ratio_fixed' to be"\
+                                 " set and in (0, 1) range, got {}"\
+                                 .format(dropout_mode, ratio_fixed))
+            self.ratio_fixed = ratio_fixed
+            self.dropped_dimsize = int(emb_size - emb_size*ratio_fixed)
+            tot_idx = np.arange(emb_size)
+            sel_idx = np.sort(np.random.choice(tot_idx, 
+                              size=self.dropped_dimsize, replace=False))
+
+        elif dropout_mode == 'fixed_given':
+
+            self.dropout_fixed = True
+            if range_fixed is None or \
+                 not isinstance(range_fixed, str) or \
+                 len(range_fixed.split(":")) < 2:
+                raise ValueError("{} mode needs 'range_dropped' to be"\
+                                 " set (i.e. 10:20)".format(dropout_mode))
+            rng = range_fixed.split(":")
+            beg = int(rng[0])
+            end = int(rng[1])
+            assert beg < end and end <= emb_size, (
+                "Incorrect range {}".format(range_fixed)
+            )
+            self.dropped_dimsize = int(emb_size - (end -beg))
+            tot_idx = np.arange(emb_size)
+            fixed_idx = np.arange(beg, end, 1)
+            sel_idx = np.setdiff1d(tot_idx, fixed_idx, assume_unique=True)
+
+        if self.dropout_fixed:
+            print ("Enabled dropout mode: {}. Selected indices to apply dropout are: {}"\
+                    .format(dropout_mode, sel_idx))
+            self.dindexes = torch.LongTensor(sel_idx) 
+            self.p = p
+            self.p_scale = 1. / (1. - self.p)
+        else:
+            # notice, it is up to the user to make sure experiments between
+            # fixed dropout and regular one are comparabe w.r.t p (i.e. for
+            # fixed mode we only keep droping a subset of units (i.e. 50%),
+            # thus p is effectively lower when compared to regular dropout)
+            self.p =  p
+            print ("Using std dropout")
+    
+    def forward(self, x):
+
+        if self.p == 0 or not self.training:
+            return x
+
+        if self.dropout_fixed and self.training:
+            self.dindexes = self.dindexes.to(x.device)
+            assert len(x.size()) == 3, (
+                "Expected to get 3 dimensional tensor, got {}"\
+                   .format(len(x.size()))
+            )
+            bsize, _, tsize = x.size()
+            #print (bsize, esize, tsize)
+            if self.drop_whole_channels:
+                probs = torch.full(size=(bsize, self.dropped_dimsize),
+                                  fill_value=1.-self.p)
+                b = Binomial(total_count=1, probs=probs)
+                mask = b.sample()
+                #print ('mask dc', mask)
+                #print ('maks dcv', mask.view(bsize, self.dropped_dimsize, -1))
+                x[:,self.dindexes,:] *= (self.p_scale * mask.view(bsize, 
+                                                  self.dropped_dimsize, -1))
+            else:
+                probs = torch.full(size=(bsize, self.dropped_dimsize, tsize), 
+                                 fill_value=1.-self.p)
+                b = Binomial(total_count=1, probs=probs)
+                mask = b.sample()
+                #print ('mask', mask)
+                x[:,self.dindexes,:] *= (mask * self.p_scale) 
+            return x
+        else:
+            return F.dropout(x, p=self.p, training=self.training)
+
 class MLPBlock(NeuralBlock):
 
-    def __init__(self, ninp, fmaps, din=0.5, dout=0, context=1, 
-                 tie_context_weights=False, name='MLPBlock'):
+    def __init__(self, ninp, fmaps, dout=0, context=1, 
+                 tie_context_weights=False, name='MLPBlock',
+                 **kwargs):
         super().__init__(name=name)
         self.ninp = ninp
         self.fmaps = fmaps
         self.tie_context_weights = tie_context_weights
         assert context % 2 != 0, context
         if tie_context_weights:
-            #print ("!!!!!!!!!!!!!!!!!!!!!!!!!!!! Tied context weigts.")
             self.W = nn.Conv1d(ninp, fmaps, 1)
             self.pool = nn.AvgPool1d(kernel_size=context, stride=1,
                                       padding=context//2, count_include_pad=False)
         else:
             self.W = nn.Conv1d(ninp, fmaps, context, padding=context//2)
 
-        self.din = FixedPatternDropout(self.ninp, p=din)
+        self.din = PatternedDropout(p=din)
         self.act = nn.PReLU(fmaps)
         self.dout = nn.Dropout(dout)
 
