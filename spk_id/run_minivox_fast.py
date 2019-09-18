91b75c19f342d2b71ba6652bce1c1db2223f8c0d
Changmin Choi <cmchoi9901@gmail.com>
2019-09-18 11:29:46 +0900

    use os.popen instead of making tmp file

--- spk_id/run_minivox_fast.py
+++ spk_id/run_minivox_fast.py
@@ -25,8 +25,8 @@ import soundfile as sf
 
 def get_freer_gpu(trials=10):
 	for j in range(trials):
-		os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
-		memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
+		f = os.popen('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free')
+		memory_available = [int(x.split()[2]) for x in f.readlines()]
 		dev_ = torch.device('cuda:'+str(np.argmax(memory_available)))
 		try:
 			a = torch.rand(1).cuda(dev_)
