fix: data leak in timit fast inference for training data fet extraction

ASR/run_TIMIT_fast.py
@@ -106,7 +106,7 @@ for wav_file in tr_lst:
     signal = signal.astype(np.float32)
     
     fea_id=wav_file.split('/')[-2]+'_'+wav_file.split('/')[-1].split('.')[0]
-    fea[fea_id]=torch.from_numpy(signal).float().to(device).view(1,1,-1)
+    fea[fea_id]=torch.from_numpy(signal).float().view(1,1,-1)
 
 
 # reading the dev signals